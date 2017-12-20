575cc84862beb48abef5f0d2e0fe7431ee5ddca7
GuangWong <guangwong@gmail.com>
2017-12-20 21:56:27 +0800

    style: comment

--- pageSnapshot/procfile.js
+++ pageSnapshot/procfile.js
@@ -4,11 +4,9 @@ module.exports = function(pandora) {
     重要前提:
     Pandora.js 中已经默认包含 worker 和 background 的进程定义
     ，只要往里面注册 Service 就能激活。
-        > pandora.process('worker').scale(pandora.dev ? 1: 'auto');
-        > pandora.process('background').scale(1);
+      pandora.process('worker').scale(pandora.dev ? 1: 'auto');
+      pandora.process('background').scale(1);
   */
-
-  //
 
   pandora
     .process('background')
